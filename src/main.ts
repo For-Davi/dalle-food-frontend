7fbfac7b41afed14789e59da31d24cb89ab966c0
Carlos Davi <carlosdavi004@gmail.com>
2025-03-27 23:46:34 -0300

    Criando primeiro form de login

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import './scss/styles.scss';
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css'; // Importar os ícones
 
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        iconfont: 'mdi', // Definir que o Vuetify deve usar a biblioteca MDI
+    },
 });
 
 const app = createApp(App);
--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
+
 defineOptions({
     name: 'Auth',
+});
+
+const dataLogin = reactive({
+    email: '' as string,
+    password: '' as string,
 });
 </script>
 
 <template>
-    <main>Auth</main>
+    <main class="container-full d-flex justify-center align-center bg-grey-darken-4">
+        <v-card class="form-auth" :loading="false" variant="tonal">
+            <v-img height="200px" src="src/assets/logos/1.png"></v-img>
+            <v-card-title> Login </v-card-title>
+            <v-form>
+                <v-text-field
+                    v-model="dataLogin.email"
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    prepend-inner-icon="mdi-email"
+                    required
+                    autocomplete="new-email"
+                    name="email"
+                ></v-text-field>
+                <v-text-field
+                    v-model="dataLogin.password"
+                    label="Senha"
+                    type="password"
+                    variant="outlined"
+                    prepend-inner-icon="mdi-send"
+                    required
+                    autocomplete="new-password-login"
+                    name="password-login"
+                ></v-text-field>
+            </v-form>
+            <v-card-actions>
+                <v-btn>Click me</v-btn>
+            </v-card-actions>
+        </v-card>
+    </main>
 </template>
