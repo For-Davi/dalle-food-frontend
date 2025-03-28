3030c36aa4e2d762044a0c48fa0beaf509ae97e9
Carlos Davi <carlosdavi004@gmail.com>
2025-03-27 22:22:08 -0300

    Build: Instalação do Vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { createPinia } from 'pinia'
 import './scss/styles.scss'
 import App from './App.vue'
 import router from './router';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 
+const vuetify = createVuetify({
+    // Configurações opcionais, como temas, diretivas, etc.
+})
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(router);
 app.use(pinia)
 app.mount('#app')
